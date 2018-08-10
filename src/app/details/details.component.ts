aae742248b8829cb26c7e1f824675efa3de758d1
Kelly Villalobos <kvdesign1978@gmail.com>
2018-08-10 08:10:34 -0500

    finished exercise with button toggle hidden content and log of button click with conditional styling of blue background and white text on fifth click

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {style} from '@angular/animations';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  public show: boolean = false;
+  public show = false;
   public buttonName: any = 'Show Password';
+  public counter = 0;
+  public count = [];
 
   constructor() {
   }
@@ -17,6 +20,20 @@ export class DetailsComponent implements OnInit {
 
   showPassword() {
     this.show = !this.show;
+    if (this.show) {
+      this.buttonName = 'Hide Password';
+    } else {
+      this.buttonName = 'Show Password';
+    }
+    this.count.push(this.count.length + 1);
+    this.counter++;
+    console.log(this.count);
+    console.log(this.counter);
   }
 
+    getColor () {
+  if (this.counter >= 5) {
+
+  }
+}
 }
